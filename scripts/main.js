8dc4d32ae3d44824efa28ab14cefaac039234aeb
Hofry1 <roman4ds@gmail.com>
2022-07-11 12:47:49 +0300

    game logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,71 @@
 function init() {
-
+    for (let i = 0; i < cardsNum; i++) {
+        cards[i].innerHTML = wordBoard[i];
+        cards[i].classList.add(colorBoard[i]);
+    }
 }
 
-function cardClick() {
-    
+function cardClick(evt) {
+    if (activeSpymaster == 1) {
+        return;
+    }
+    if (playingTeam == "r") {
+        if (colorBoard[evt.target.id] == "r") {
+            cards[evt.target.id].classList.add("shown");
+            return;
+        }
+        if (colorBoard[evt.target.id] == "b") {
+            cards[evt.target.id].classList.add("shown");
+            flipTurn();
+            return;
+        }
+        if (colorBoard[evt.target.id] == "n") {
+            cards[evt.target.id].classList.add("shown");
+            flipTurn();
+            return;
+        }
+        if (colorBoard[evt.target.id] == "a") {
+            cards[evt.target.id].classList.add("shown");
+            endGame();
+            return;
+        }
+    }
+    if (colorBoard[evt.target.id] == "r") {
+        cards[evt.target.id].classList.add("shown");
+        flipTurn();
+        return;
+    }
+    if (colorBoard[evt.target.id] == "b") {
+        cards[evt.target.id].classList.add("shown");
+        return;
+    }
+    if (colorBoard[evt.target.id] == "n") {
+        cards[evt.target.id].classList.add("shown");
+        flipTurn();
+        return;
+    }
+    if (colorBoard[evt.target.id] == "a") {
+        cards[evt.target.id].classList.add("shown");
+        endGame();
+        return;
+    }
+    return;
 }
 
 function showSpymaster() {
     if (activeSpymaster == 0) {
         spymaster.innerHTML = "Показать игровую доску";
         activeSpymaster = 1;
+        for (let i = 0; i < cardsNum; i++) {
+            cards[i].classList.add("spymaster");
+        }
         return;
     }
     spymaster.innerHTML = "Доска спаймастера";
     activeSpymaster = 0;
-    return;
+    for (let i = 0; i < cardsNum; i++) {
+        cards[i].classList.remove("spymaster");
+    }
 }
 
 function colorsAssignment() {
@@ -29,16 +80,41 @@ function newGame() {
     
 }
 
+function endGame() {
+    
+}
+
+function flipTurn() {
+    if (playingTeam == "r") {
+        playingTeam = "b";
+        teamTurn.innerHTML = "Ход синих";
+        team.style.background = "blue";
+        return;
+    }
+    playingTeam = "r";
+    teamTurn.innerHTML = "Ход красных";
+    team.style.background = "red";
+}
+
+let colorBoard = ["r", "r", "r", "r", "n", "r", "a", "r", "r", "r", "b", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r"]
+let wordBoard = ["hi", "", "", "", "", "", "", "", "", "hi", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+
 let activeSpymaster = 0;
 const cardsNum = 25;
-const redNum = 9;
-const blueNum = 8;
-let playingTeam = "red";
+let redNum = 9;
+let blueNum = 8;
+let playingTeam = "r";
 const grid = document.getElementsByClassName("grid-container")[0];
 const cards = grid.children;
 const spymaster = document.getElementById("spymaster-button");
-spymaster.addEventListener('click', showSpymaster)
+const team = document.getElementsByClassName("team")[0];
+const teamTurn = document.getElementById("team-turn");
+const redScore = document.getElementById("red-score");
+const blueScore = document.getElementById("blue-score");
+spymaster.addEventListener('click', showSpymaster);
 
 for (let i = 0; i < cardsNum; i++) {
-    cards[i].onclick = cardClick();
+    cards[i].onclick = cardClick;
 }
+
+init();
